6e69106cbf5c6210a3f86a53dc771f5280db1156
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-12-10 14:05:33 -0800

    Adds routes to get and post notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,37 @@
 const notes = require('express').Router();
-const {readFromFile} = require('../helpers/fsHelper');
+const {readFromFile, readAndAppend} = require('../helpers/fsHelper');
 
-notes.get('/api', (req, res) => {
-    console.log(res);
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  })
+
+notes.get('/:noteTitle', (req, res) => {
+    const noteTitle = req.params.title;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => tip.title === noteTitle);
+        console.log(result);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('NA');
+      });
   });
+  
 
-// notes.post('/', (req, res) => {
-//     readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
-//     console.log(data);
-//   }); 
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 
 module.exports = notes;
